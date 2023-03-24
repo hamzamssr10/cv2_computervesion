d2d1e98a7f337df7b2db42b762a2b74b9b57ef47
hamzamssr10 <hamzamassar10@gmail.com>
2023-03-24 11:17:32 +0000

    chapter 1 = reading an images or vedio

--- chpt1.py
+++ chpt1.py
@@ -14,9 +14,9 @@ while True:
         break
 #################################################################
 cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-cap.set(10,100)
+cap.set(3,640) # width
+cap.set(4,480) # height
+cap.set(10,100) # brightness
 
 
 while True:
